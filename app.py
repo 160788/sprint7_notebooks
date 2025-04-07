190eb6fb50a827afec5fcca9b7e9bc9d2f9f53a4
Cele <celestequiroga425@gmail.com>
2025-04-07 21:23:42 +0100

    Mi primer proyecto

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-df=pd.read_csv("../vehicles_us.csv")
+df=pd.read_csv("vehicles_us.csv")
 
 hist_button = st.button('Construir histograma') # crear un botón
         
