3d86f396c5bfa18153cee07cabdab78a4bb6ee71
Cele <celestequiroga425@gmail.com>
2025-04-07 22:35:35 +0100

    -

--- app.py
+++ app.py
@@ -30,11 +30,5 @@ if hist_button: # al hacer clic en el botón
     else:
         st.write("Las columnas necesarias para el gráfico no están disponibles")
 
-[server]
-headless = true
-port = 10000
 
-[browser]
-serverAddress = "0.0.0.0"
-serverPort = 10000
         
